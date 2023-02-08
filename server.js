a6c71b6d06e23e4ef84f489d80e0e67309c85831
fil-felipe <filip.adamek@outlook.com>
2023-02-08 10:48:39 +0100

    add cors to server

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const Firestore = require('@google-cloud/firestore');
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 const db = new Firestore({
