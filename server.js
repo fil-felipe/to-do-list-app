6e344f3d466b0f0505136497b010a96b2ac822ec
fil-felipe <filip.adamek@outlook.com>
2023-02-27 10:06:56 +0100

    DB tasks for specific collections

--- server.js
+++ server.js
@@ -14,9 +14,16 @@ const db = new Firestore({
 //   keyFilename: 'application_default_credentials.json'
 });
 
-let collection_name = "default-to-do-list"
+let defaul_collection_name = "default-to-do-list"
 
-const task_collection = db.collection(collection_name)
+const task_collection = db.collection(defaul_collection_name)
+const user_collection = db.collection("users");
+
+async function readUserData(user_name) {
+    const output = await user_collection.doc(user_name).get()
+    const output_data = output.data()
+    return output_data
+}
 
 async function readData() {
     const output = []
@@ -30,20 +37,53 @@ async function readData() {
     return output
 }
 
+async function readDataFromCollection(collection_name) {
+    const collectionDb = db.collection(collection_name)
+    const output = []
+    const snapshot = await collectionDb.get()
+    snapshot.forEach(doc => {
+        output.push({
+            id: doc.id,
+            content: doc.data()
+        })
+        })
+    return output
+}
+
+
+
+app.get("/user", async function(req,res) {
+    const output = await readUserData("default_user");
+    res.json(output);
+})
+
 app.get("/", function(req,res){
     // addData()
     // readData()
     res.send("Welcome in to-do-list-app");
 });
 
-app.get("/read-task", function(req,res){
-    readData().then(response => (res.json(response)));
+app.get("/read-task/", function(req,res){
+    readDataFromCollection(defaul_collection_name).then(response => (res.json(response)));
+});
+
+app.get("/read-task/:collection", function(req,res){
+    readDataFromCollection(req.params.collection).then(response => (res.json(response)));
 });
 
 app.post("/add-task", async function(req,res) {
     const json_data = req.body;
     await task_collection.add(json_data);
-    const output = await readData();
+    const output = await readDataFromCollection(defaul_collection_name);
+    res.json(output);
+})
+
+app.post("/add-task/:collection", async function(req,res) {
+    const collectionName = req.params.collection
+    const collection = db.collection(collectionName)
+    const json_data = req.body;
+    await collection.add(json_data);
+    const output = await readDataFromCollection(collectionName);
     res.json(output);
 })
 
@@ -61,15 +101,43 @@ app.post("/delete-task", async function(req,res){
         res.json(output);
 });
 
+app.post("/delete-task/:collection", async function(req,res){
+    const collectionName = req.params.collection
+    const collection = db.collection(collectionName)
+    const input_json = req.body;
+    const current_data = await collection.doc(input_json.id).get();
+    let updated_data = current_data.data();
+    if (updated_data.deleted) {
+        updated_data.deleted = false;
+    } else {
+        updated_data.deleted = true;
+    }
+    await collection.doc(input_json.id).set(updated_data);
+    const output = await readDataFromCollection(collectionName);
+    res.json(output);
+});
+
 app.delete("/delete-tasks", async function(req,res) {
     const actualTasks = await task_collection.get();
-
     await actualTasks.forEach(doc => {
             if (doc.data().deleted) {
                 task_collection.doc(doc.id).delete();
             }
         });
     const output = await readData();
+    res.json(output);
+});
+
+app.delete("/delete-tasks/:collection", async function(req,res) {
+    const collectionName = req.params.collection
+    const collection = db.collection(collectionName)
+    const actualTasks = await collection.get();
+    await actualTasks.forEach(doc => {
+            if (doc.data().deleted) {
+                collection.doc(doc.id).delete();
+            }
+        });
+    const output = await readDataFromCollection(collectionName);
     res.json(output);
 });
 
